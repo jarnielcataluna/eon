2154de65b87e5182d1c691308adf40f4fc5ff904
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2016-11-16 11:20:55 +0800

    add co creator page

--- web/js/inner.js
+++ web/js/inner.js
@@ -29,17 +29,49 @@ $(document).ready(function() {
 		nextText: "&nbsp;"
 	});
 
+	// ClOSE MODAL
 	$('.pop-close, .bg-close').click(function(e){
 		e.preventDefault();
 		$('.popup-wrap').removeClass('active');
 	});
 
+	// FAQ TABS
+	$('.faq-categ-wrap a').click(function(){
+		var _this = $(this).attr('href');
+		
+		$('.faq-categ-wrap li').removeClass('active');
+		$('.faq-categ-content').css({'display': 'none'});
+		$(this).closest('li').addClass('active');
+		$(_this).css({'display' : 'block'});
+	});
+	
+	$('.fc-question').click(function(){
+		$(this).closest('li').toggleClass('active');
+		$(this).closest('li').find('.fc-answer').stop(true, false).slideToggle(300);
+	});
+
+	// OPEN MODAL
 	$('.btn-modal').click(function(e){
 		e.preventDefault();
 
 		$('.popup-content').css({'top' : _cur_top + 40});
 		$('.popup-wrap').addClass('active');
 	});	
+
+	$('.btn-co-create').click(function(e){
+		e.preventDefault();
+		$(this).hide(300);
+		$('#photoupload-form').stop(true, false).slideDown(300);
+	});
+
+	$('.btn-close-form').click(function(e){
+		e.preventDefault();
+		$('#photoupload-form').stop(true, false).slideUp(300);
+		setTimeout(function(){
+			$('.btn-co-create').show();
+		}, 300);
+		
+	});
 });
 
 win.load(function() {
